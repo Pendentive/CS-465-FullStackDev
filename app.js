7478e852d61ff54439e3809e624440dbf834da86
Pendentive <72536999+Pendentive@users.noreply.github.com>
2024-11-26 17:51:29 -0600

    Refactor server backend to use mongoDB

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// Define routers
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
 var travelRouter = require('./app_server/routes/travel');
@@ -12,8 +13,12 @@ var adminRouter = require('./app_server/routes/admin');
 var checkoutRouter = require('./app_server/routes/checkout');
 var newsRouter = require('./app_server/routes/news');
 var reservationsRouter = require('./app_server/routes/reservations'); 
+var apiRouter = require('./app_api/routes/index');
 
 var handlebars = require('hbs');
+
+// Bring in the database
+require('./app_api/models/db');
 
 var app = express();
 
@@ -21,7 +26,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'hbs');
 
-// register handlebars partials (https://www.npmjs.com/package/hbs)
+// Register handlebars partials (https://www.npmjs.com/package/hbs)
 handlebars.registerPartials(__dirname + '/app_server/views/partials');
 
 app.use(logger('dev'));
@@ -30,6 +35,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Wire routes to controllers
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/travel', travelRouter);
@@ -38,6 +44,7 @@ app.use('/admin', adminRouter);
 app.use('/checkout', checkoutRouter);
 app.use('/news', newsRouter);
 app.use('/reservations', reservationsRouter);
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
