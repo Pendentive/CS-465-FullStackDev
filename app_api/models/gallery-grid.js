e1e33aa8cc911152f979c8401bd684cb2155ca03
Pendentive <72536999+Pendentive@users.noreply.github.com>
2025-02-07 18:09:19 -0600

    Remove redundant attributes in sub models

--- app_api/models/gallery-grid.js
+++ app_api/models/gallery-grid.js
@@ -2,11 +2,6 @@ const mongoose = require('mongoose');
 const BaseModel = require('./base');
 
 const galleryGridSchema = new mongoose.Schema({
-    title: { 
-        type: String, 
-        required: true 
-    },
-    description: String,
     images: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Image'
--- app_api/models/type-intro.js
+++ app_api/models/type-intro.js
@@ -2,11 +2,6 @@ const mongoose = require('mongoose');
 const BaseModel = require('./base');
 
 const typeIntroSchema = new mongoose.Schema({
-    title: { 
-        type: String, 
-        required: true 
-    },
-    description: String,
     leftPadding: {
         type: Number,
         required: true,
