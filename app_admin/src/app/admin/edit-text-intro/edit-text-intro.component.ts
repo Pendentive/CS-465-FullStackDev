25b2c85525fb9f3cccf35a03c10225df1877ff64
Pendentive <72536999+Pendentive@users.noreply.github.com>
2025-02-09 14:38:42 -0600

    Refactored edit-text to use material. Standerdized css classes

--- app_admin/src/app/admin/edit-text-intro/edit-text-intro.component.ts
+++ app_admin/src/app/admin/edit-text-intro/edit-text-intro.component.ts
@@ -1,25 +1,56 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnChanges, SimpleChanges } from '@angular/core';
 import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { ComponentService } from '../../services/component.service';
 
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+
 @Component({
   selector: 'app-edit-text-intro',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule
+  ],
   templateUrl: './edit-text-intro.component.html',
   styleUrls: ['./edit-text-intro.component.css']
 })
-export class EditTextIntroComponent implements OnInit {
-  @Input() formGroup!: FormGroup;   // WAS PUBLIC
+export class EditTextIntroComponent implements OnInit, OnChanges {
+  @Input() formGroup!: FormGroup;
   @Input() componentId!: string;
+  componentTitle: string = '';
 
   constructor(private componentService: ComponentService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.setComponentTitle();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['formGroup']) {
+      this.setComponentTitle();
+    }
+  }
+
+  private setComponentTitle(): void {
+    if (this.formGroup && this.formGroup.get('kind')?.value) {
+      this.componentTitle = this.formGroup.get('kind')?.value + ' Component';
+    } else {
+      this.componentTitle = 'Component';
+    }
+  }
 
   onSubmit(): void {
-    if (this.formGroup.valid) {  
+    if (this.formGroup.valid) {
       this.componentService.updateTypeIntro(this.componentId, this.formGroup.value).subscribe({
         next: () => {
           console.log('TypeIntro component updated successfully');
